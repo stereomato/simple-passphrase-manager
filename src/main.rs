00ec1486a4fbb10b55482f5a5ceec00b52014f48
pearsche <thepearsche@proton.me>
2023-05-30 12:13:31 -0500

    properly rewrite count_words, and, also rewrite comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::{fs};
 
 
-// essentially getline from c++
-// returns the word itself
+// Returns the word itself.
 fn get_word(input_string: &String, linecount: usize) -> &str {
 	let string_as_bytes = input_string.as_bytes();
 	// This value helps with skipping lines
@@ -12,12 +11,10 @@ fn get_word(input_string: &String, linecount: usize) -> &str {
 		if item == b'\n' {
 			// Return for any word that isn't the last.
 			if helper == 1 {
-				println!("a");
 				return &input_string[word_start_index..index];	
 			}
 			// This helps skip to the word we intend to get, and sets the word_start_index to what it should be.
 			if helper > 1 {
-				println!("b");
 				helper -= 1;
 				word_start_index = index + 1;
 			}
@@ -27,19 +24,21 @@ fn get_word(input_string: &String, linecount: usize) -> &str {
 	return &input_string[word_start_index..];
 }
 
-// needs to be fixed still
+// Counts the words.
 fn count_words(input_string: &String) -> usize {
 	let string_as_bytes = input_string.as_bytes();
 	let mut counter = 0;
+	let mut last_character = 0;
 	for (_i, &item) in string_as_bytes.iter().enumerate() {
-		if item == b'\n'  {
+		// If the current character is a newline, but the last one isn't a newline, add 1 to the counter.
+		if item == b'\n' && last_character != b'\n' {
 			counter += 1;
-		}	
+		}
+		last_character = item;
+
 	}
-	// When the EOF is detected (because the previous for loop ended) and counter is more than zero (because there's at least one word, add 1 to the counter)
-	// Not actually detecting the EOF, but hey, I'm still learning.
-	// Also, doesn't work if there's just one word without a newline.
-	if counter > 0 {
+	// If the last character isn't empty nor it is a newline, add 1 to the counter (to start from 1).
+	if last_character != 0 && last_character != b'\n' {
 		counter += 1;
 	}
 	return counter;
@@ -50,7 +49,7 @@ fn main() {
 	
 	let dictionary_contents = fs::read_to_string(dictionary_file).expect("Couldn't read file.");
 
-	let test = get_word(&dictionary_contents, 1);
+	let test = get_word(&dictionary_contents, 61);
 	let test_word = test;
 	println!("The word is {}.", test_word);
 
